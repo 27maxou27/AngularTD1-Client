6804c4dcbe70d9bea314a7e38628bedfb0102d40
mat <mat@mat.fz>
2019-12-10 14:55:51 +0100

    zbeub

--- src/app/components/account/account.component.ts
+++ src/app/components/account/account.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { LoginService } from '../../services/login.service';
 
 @Component({
   selector: 'app-account',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account.component.scss']
 })
 export class AccountComponent implements OnInit {
+  pseudo : string
+  
 
-  constructor() { }
+  loginForm = new FormGroup ({
+    firstname: new FormControl('', Validators.required),
+    lastname: new FormControl('', Validators.required),
+    
+  });
+
+  get f() { return this.loginForm.controls; }
+
+
+  constructor(private loginService : LoginService) { }
 
   ngOnInit() {
+    var user = this.loginService.currentUserValue;
+
+    this.pseudo = user.pseudo;
+
+    
+    
+    
+    
   }
 
 }
